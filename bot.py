1c740dfbd98deffb125eff7d4a45667c637e45d0
Diana Mukhanova <diananova2508@gmail.com>
2020-11-17 21:44:31 +0100

    added github API

--- bot.py
+++ bot.py
@@ -1,13 +1,33 @@
 import os
-
+from github import Github
+from pprint import pprint
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+g = Github(GITHUB_TOKEN)
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='!')
 
-bot = discord.Client()
+# get current user
+user = g.get_user("Laurell876")
+print(user.name)
+#repo = g.get_repo("MartinHeinz/python-project-blueprint")
 
+#for repo in g.get_user().get_repos():
+#    print(repo.name)
+
+@bot.command(name='git')
+async def test(ctx):
+	response = 'hey dirtbag'
+	await ctx.send(response)
+
+#discord.Client()
+
+'''
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
@@ -16,4 +36,5 @@ async def on_ready():
 async def on_message(message):
 	if message.content == "hello":
 		await message.channel.send("hey dirtbag")
-bot.run(TOKEN)
+'''
+bot.run(DISCORD_TOKEN)
